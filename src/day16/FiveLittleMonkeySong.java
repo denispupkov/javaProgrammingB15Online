e76187eeea21ba4f80899f3961c14de94be95bd8
Denis Pupkov <denis.pupkov@gmail.com>
2019-12-01 10:38:54 -0500

    Monkey song

---
+++ src/day16/FiveLittleMonkeySong.java
@@ -0,0 +1,21 @@
+package day16;
+
+public class FiveLittleMonkeySong {
+
+    public static void main(String[] args) {
+
+        int monkey=5;
+
+        while (monkey>=1) {
+
+            System.out.println(monkey+" little monkeys jumping on the bed,\n" +
+                    "One fell down and bumped his head,\n" +
+                    "Mama called the doctor and the doctor said,\n" +
+                    "No more monkeys jumping on the bed!");
+            --monkey;
+        }
+        System.out.println("Put those monkeys right to bed!");
+
+
+    }
+}
