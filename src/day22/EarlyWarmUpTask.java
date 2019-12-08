d377afd01b6e88cd32231b9c0aa7f038a2eae326
Denis Pupkov <denis.pupkov@gmail.com>
2019-12-08 11:14:01 -0500

    day 22

---
+++ src/day22/EarlyWarmUpTask.java
@@ -0,0 +1,23 @@
+package day22;
+
+public class EarlyWarmUpTask {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 12; i++) {
+
+            //System.out.println("i= "+i);
+            for (int x = 1; x <=12 ; x++) {
+                System.out.println(i+"x"+x+"="+i*x);
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
